48794854809318e39959bdf78174abe49b336f5c
Abhishek pp <abhishekpp.bluecast@gmail.com>
2024-07-31 10:27:31 +0530

    initial commit

--- heroku/index.js
+++ heroku/index.js
@@ -6,35 +6,76 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+const { default: axios } = require('axios');
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var xhub = require('express-x-hub');
+require('dotenv').config();
 
-app.set('port', (process.env.PORT || 5000));
+const verify_token = process.env.APP_VERIFY_TOKEN;
+const port = process.env.PORT || 5000;
+const appId =  process.env.APP_ID;
+const appSecret = process.env.APP_SECRET;
+const verifyToken = process.env.VERIFY_TOKEN;
+const appAccessToken = process.env.APP_ACCESS_TOKEN;
+const appCallbackUrl = process.env.APP_CALLBACK_URL;
+
+app.set('port', (port));
 app.listen(app.get('port'));
 
 app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
-var token = process.env.TOKEN || 'token';
-var received_updates = [];
+const received_updates = [];
 
 app.get('/', function(req, res) {
   console.log(req);
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
 
-app.get(['/facebook', '/instagram'], function(req, res) {
+app.get('/facebook', function(req, res) {
   if (
     req.query['hub.mode'] == 'subscribe' &&
-    req.query['hub.verify_token'] == token
+    req.query['hub.verify_token'] == verify_token
   ) {
     res.send(req.query['hub.challenge']);
   } else {
     res.sendStatus(400);
   }
 });
+
+// SET UP WEBHOOK
+// const appId =  process.env.APP_ID;
+// const appAccessToken = process.env.APP_ACCESS_TOKEN;
+// const webhookConfig = {
+//   object: 'page', // Replace with 'user' if you want to subscribe to user updates
+//   callback_url: process.env.APP_CALLBACK_URL,
+//   fields: 'about,picture', // Replace with the fields you want to subscribe to
+//   include_values: true,
+//   verify_token: process.env.APP_VERIFY_TOKEN
+// };
+
+// const postWebhookSubscription = async () => {
+//   try {
+//     const response = await axios.post(
+//       `https://graph.facebook.com/v20.0/${appId}/subscriptions`,
+//       null, // POST data is sent in query params, not the body
+//       {
+//         params: webhookConfig,
+//         headers: {
+//           'Authorization': `Bearer ${appAccessToken}`
+//         }
+//       }
+//     );
+
+//     console.log('Response:', response.data);
+//   } catch (error) {
+//     console.error('Error:', error.response ? error.response.data : error.message);
+//   }
+// };
+
+// postWebhookSubscription();
 
 app.post('/facebook', function(req, res) {
   console.log('Facebook request body:', req.body);
@@ -51,12 +92,11 @@ app.post('/facebook', function(req, res) {
   res.sendStatus(200);
 });
 
-app.post('/instagram', function(req, res) {
-  console.log('Instagram request body:');
-  console.log(req.body);
-  // Process the Instagram updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
 
-app.listen();
+setInterval(() => {
+  console.log(received_updates);
+}, 5000);
+
+app.listen((address, error)=>{
+  console.log("Server started on port"+ 5000);
+});
